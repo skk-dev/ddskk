d2f60eb179b1d4be829c78e388fc3be9d5b4c37e
minakaji <minakaji>
1999-08-30 10:24:21 +0000

    *** empty log message ***

--- make.bat
+++ make.bat
@@ -4,9 +4,9 @@ rem Copyright (C) 1999 Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem
 rem Author: Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem Maintainer: Mikio Nakajima, mailto:minakaji@osaka.email.ne.jp
-rem Version: $Id: make.bat,v 1.2 1999/08/19 12:58:32 minakaji Exp $
+rem Version: $Id: make.bat,v 1.3 1999/08/30 10:24:21 minakaji Exp $
 rem Created: March 23, 1999
-rem Last Modified: $Date: 1999/08/19 12:58:32 $
+rem Last Modified: $Date: 1999/08/30 10:24:21 $
 
 rem ********************************************************************
 rem *                                                                  *
@@ -21,4 +21,5 @@ set V_S_LISPDIR=NONE
 rem ********************************************************************
 
 %EMACS% -batch -q -no-site-file -l SKK-MK -f install-skk %PREFIX% %LISPDIR% %V_S_LISPDIR%
+cp skk-autoloads.el %LISPDIR%(J\(Bskk
 
