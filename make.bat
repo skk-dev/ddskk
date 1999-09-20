f7625743bf4647b756dddee2b2d189215848de14
minakaji <minakaji>
1999-09-20 03:12:12 +0000

    *** empty log message ***

--- make.bat
+++ make.bat
@@ -4,9 +4,9 @@ rem Copyright (C) 1999 Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem
 rem Author: Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem Maintainer: Mikio Nakajima, mailto:minakaji@osaka.email.ne.jp
-rem Version: $Id: make.bat,v 1.6 1999/09/16 14:06:41 minakaji Exp $
+rem Version: $Id: make.bat,v 1.7 1999/09/20 03:12:12 minakaji Exp $
 rem Created: March 23, 1999
-rem Last Modified: $Date: 1999/09/16 14:06:41 $
+rem Last Modified: $Date: 1999/09/20 03:12:12 $
 
 rem ********************************************************************
 rem *                                                                  *
@@ -14,7 +14,7 @@ rem * Please modify following five lines according to your environment *
 rem *                                                                  *
 rem ********************************************************************
 set EMACS=c:\usr\meadow\1.00\bin\meadow95.exe
-set PREFIX=NONE
+set PREFIX=c:\usr\meadow\1.00
 set LISPDIR=c:\usr\meadow\site-lisp
 set PACKAGEDIR=NONE
 set V_S_LISPDIR=NONE
