3c4db79de1153c7e0b3bfc33239aaf61cb37bf8b
yutopia <yutopia>
1999-12-16 01:13:42 +0000

    correct make.bat

--- make.bat
+++ make.bat
@@ -4,9 +4,9 @@ rem Copyright (C) 1999 Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem
 rem Author: Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
 rem Maintainer: Mikio Nakajima, mailto:minakaji@osaka.email.ne.jp
-rem Version: $Id: make.bat,v 1.11 1999/12/02 01:07:46 yutopia Exp $
+rem Version: $Id: make.bat,v 1.12 1999/12/16 01:13:51 yutopia Exp $
 rem Created: March 23, 1999
-rem Last Modified: $Date: 1999/12/02 01:07:46 $
+rem Last Modified: $Date: 1999/12/16 01:13:51 $
 
 rem ********************************************************************
 rem *                                                                  *
@@ -22,9 +22,9 @@ rem ********************************************************************
 
 set arg1=%1
 
-if %arg1% == install goto install
-if %arg1% == what-where goto listing
-if %arg1% == clean goto clean
+if "%arg1%"=="install" goto install
+if "%arg1%"=="what-where" goto listing
+if "%arg1%"=="clean" goto clean
 
 :install
 %EMACS% -batch -q -no-site-file -l SKK-MK -f SKK-MK-install
