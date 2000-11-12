4d0a3da25b44c2136d6046a8cc1f5250c5586cd3
czkmt <czkmt>
2000-11-12 07:36:24 +0000

    UPdate.

--- nicola/skk-kanagaki-menu.el
+++ nicola/skk-kanagaki-menu.el
@@ -3,23 +3,21 @@
 
 ;; Author: Tetsuo Tsukamoto <czkmt@remus.dti.ne.jp>
 ;; Keywords: hardware, japanese
-;; Last Modified: $Date: 2000/11/11 03:23:42 $
 
-;; This file is not yet part of Daredevil SKK.
+;; This file is part of SKK (Simple Kana to Kanji conversion program).
 
-;; Daredevil SKK  is free software;  you  can redistribute it  and/or modify it
-;; under the terms  of the GNU General Public License  as published by the Free
-;; Software  Foundation;  either versions  2,  or  (at your option)  any  later
-;; version.
+;; SKK  is free software;  you  can redistribute it  and/or modify it under the
+;; terms  of the GNU General Public License  as published  by the Free Software
+;; Foundation;  either versions  2,  or  (at your option)  any  later version.
 
-;; Daredevil SKK is distributed in the hope that it will be useful  but WITHOUT
-;; ANY  WARRANTY;  without  even  the implied  warranty  of MERCHANTABILITY  or
-;; FITNESS  FOR  A PARTICULAR PURPOSE.  See the GNU General Public License  for
-;; more details.
+;; SKK  is distributed  in the hope  that  it will  be useful  but  WITHOUT ANY
+;; WARRANTY;  without even the implied  warranty  of MERCHANTABILITY or FITNESS
+;; FOR  A  PARTICULAR  PURPOSE.  See  the  GNU General Public License  for more
+;; details.
 
 ;; You should have received a copy of the GNU General Public License along with
-;; Daredevil SKK,  see the file COPYING.  If not,  write  to  the Free Software
-;; Foundation Inc., 59 Temple Place - Suite 330, Boston,  MA 02111-1307, USA.
+;; SKK,  see the file COPYING.  If not,  write  to the Free Software Foundation
+;; Inc., 59 Temple Place - Suite 330, Boston,  MA 02111-1307, USA.
 
 ;;; Commentary:
 
