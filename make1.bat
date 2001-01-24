59e2d13b5b1192033c3e2fc6f1cbb8a203657281
yutopia <yutopia>
2001-01-24 05:39:29 +0000

    change make.bat to makeit.bat

--- make1.bat
+++ make1.bat
@@ -1,80 +1,60 @@
-@echo off
-rem ---
-rem --- cmail install batch file for Meadow & NTEmacs
-rem ---  1999/07/07, Masaki YATSU mailto:yatsu@aurora.dti.ne.jp
-rem ---              cmail ML member
-rem ---  modified 1999/12/01, Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
-rem ---  date $Date: 2001/01/16 02:17:20 $
-rem ---  version $Id: make1.bat,v 1.1 2001/01/16 02:17:20 yutopia Exp $
+echo off
+rem MAKE1.BAT for SKK.
+rem Copyright (C) 1999 Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
+rem
+rem Author: Yuh Ohmura, mailto:yutopia@t3.rim.or.jp
+rem Maintainer: SKK Development Team mailto:skk@ring.gr.jp
+rem Version: $Id: make1.bat,v 1.2 2001/01/24 05:39:39 yutopia Exp $
+rem Created: March 23, 1999
+rem Last Modified: $Date: 2001/01/24 05:39:39 $
 
-
-rem --- 引数
-rem ---   elc : バイトコンパイルのみ。
-rem ---   all, install またはなし : インストール。
-rem ---   elc-no-options : バイトコンパイルのみ(オプションなし)
-rem ---   install-no-option : インストール(オプションなし)
-rem ---   clean : お掃除
-rem ---   what-where : インストール場所をサーチ
+rem --- argument
+rem ---   elc : byte compile
+rem ---   all, install : install
+rem ---   clean : cleaning garvage file
+rem ---   what-where : print where to install
 rem ---
 
-
-rem --- 最初に make.bat から実行されているかチェックする
+rem --- check calling from make.bat
 if not "%SUBMAKEOK%"=="OK" goto prnusage
 set SUBMAKEOK=
 
-rem --- 引数判定
+rem argument check
+
 set arg1=%1
-if "%arg1%"=="" goto install
+
 if "%arg1%"=="elc" goto compile
-if "%arg1%"=="elc-no-options" goto compilenoopt
 if "%arg1%"=="all" goto install
 if "%arg1%"=="install" goto install
-if "%arg1%"=="install-no-options" goto installnoopt
-if "%arg1%"=="clean" goto clean
 if "%arg1%"=="what-where" goto listing
-echo Unrecognized argument: specify either 'elc', 'elc-no-options', 'all',
-echo 'install', 'install-no-options', 'clean' or 'what-where'. Default is
-echo 'install' (when no argument is specified.)
-goto end
+if "%arg1%"=="clean" goto clean
+echo Unrecognized argument: specify either 'elc', 'all',
+echo 'install', 'clean' or 'what-where'.
+goto pauseend
 
-rem --- インストール
-:install
-%EMACS% -batch -q -no-site-file -l CMAIL-MK -f install-cmail %PREFIX% NONE %LISPDIR% %INFODIR% %VERSION_SPECIFIC_LISPDIR%
-goto end
-
-rem --- インストール (options を除外)
-:installnoopt
-%EMACS% -batch -q -no-site-file -l CMAIL-MK -f install-cmail-no-options %PREFIX% NONE %LISPDIR% %INFODIR% %VERSION_SPECIFIC_LISPDIR%
-goto end
-
-rem --- *.elcの作成
 :compile
-%EMACS%  -batch -q -no-site-file -l CMAIL-MK -f compile-cmail %PREFIX% NONE %LISPDIR% %INFODIR% %VERSION_SPECIFIC_LISPDIR%
+%EMACS% -batch -q -no-site-file -l SKK-MK -f SKK-MK-compile
 goto end
 
-rem --- *.elcの作成 (options を除外)
-:compilenoopt
-%EMACS%  -batch -q -no-site-file -l CMAIL-MK -f compile-cmail-no-options %PREFIX% NONE %LISPDIR% %INFODIR% %VERSION_SPECIFIC_LISPDIR%
+:install
+%EMACS% -batch -q -no-site-file -l SKK-MK -f SKK-MK-install
 goto end
 
-rem --- 掃除
-:clean
-del *.elc
-del doc\*.info
-del *~
-del apel\*~
-del alel\*.elc
-del doc\*~
-rem del icon\*~
-goto end
-
-rem --- インストール先のリスト
 :listing
-%EMACS% -batch -q -no-site-file -l CMAIL-MK -f what-where-cmail %PREFIX% NONE %LISPDIR% %INFODIR% %VERSION_SPECIFIC_LISPDIR%
+%EMACS% -batch -q -no-site-file -l SKK-MK -f SKK-MK-what-where
 goto end
 
-rem --- このファイルは単体で実行するものではない旨表示する
+:clean
+del skk-autoloads.el *.elc doc\skk.info* *~
+
+rem --- This file should not be executed by itself. Use make.bat.
 :prnusage
 echo This file should not be executed by itself. Use make.bat.
 
+rem --- If error occurd, stay display until any key is typed.
+:pauseend
+echo Type any key when you're done reading the error message.
+pause
+
 :end
+
