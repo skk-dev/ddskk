b0c25131969d34a121b4771b4c60be2ef2eda7f8
minakaji <minakaji>
2001-07-08 09:58:35 +0000

    Modified the copyright notice.

--- experimental/skk-hint.el
+++ experimental/skk-hint.el
@@ -4,7 +4,7 @@
 ;; Author: Yoshiki Hayashi <yoshiki@xemacs.org>
 ;; Keywords: japanese
 
-;; This file is part of Daredevil SKK yet.
+;; This file is part of Daredevil SKK.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
