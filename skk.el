2cc410b304d109645978629edf577ea852bb686f
czkmt <czkmt>
2001-10-20 04:16:32 +0000

    (skk-search-sagyo-henkaku): Use `skk-substring', not `substring'.

--- skk.el
+++ skk.el
@@ -5,9 +5,9 @@
 
 ;; Author: Masahiko Sato <masahiko@kuis.kyoto-u.ac.jp>
 ;; Maintainer: SKK Development Team <skk@ring.gr.jp>
-;; Version: $Id: skk.el,v 1.163 2001/10/20 02:33:11 czkmt Exp $
+;; Version: $Id: skk.el,v 1.164 2001/10/20 04:16:32 czkmt Exp $
 ;; Keywords: japanese
-;; Last Modified: $Date: 2001/10/20 02:33:11 $
+;; Last Modified: $Date: 2001/10/20 04:16:32 $
 
 ;; Daredevil SKK is free software; you can redistribute it and/or modify it
 ;; under the terms of the GNU General Public License as published by the Free
@@ -3723,7 +3723,7 @@ If you want to restore the dictionary from the disc, try
   (unless okuri-list
     (setq okuri-list '("さ" "し" "す" "せ")))
   (when (member skk-henkan-okurigana okuri-list)
-    (let ((skk-henkan-key (substring
+    (let ((skk-henkan-key (skk-substring
 			   skk-henkan-key
 			   0 (1- (skk-str-length skk-henkan-key))))
 	  skk-henkan-okurigana
