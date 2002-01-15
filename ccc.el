08a82ae68f5bba972c8383b0b680f71f959ef49b
czkmt <czkmt>
2002-01-15 11:40:47 +0000

    (update-buffer-local-frame-params): Abolish optional argument 	FORCE-CURRENT-BUFFER. New optional argument BUFFER.
    	(minibuffer-exit-hook): Don't bother setting buffer.

--- ccc.el
+++ ccc.el
@@ -4,9 +4,9 @@
 
 ;; Author: Masatake YAMATO <masata-y@is.aist-nara.ac.jp>
 ;; Maintainer: SKK Development Team <skk@ring.gr.jp>
-;; Version: $Id: ccc.el,v 1.19 2002/01/14 06:34:02 czkmt Exp $
+;; Version: $Id: ccc.el,v 1.20 2002/01/15 11:40:47 czkmt Exp $
 ;; Keywords: cursor
-;; Last Modified: $Date: 2002/01/14 06:34:02 $
+;; Last Modified: $Date: 2002/01/15 11:40:47 $
 
 ;; This file is not part of GNU Emacs.
 
@@ -133,10 +133,10 @@
 	 (string= a b)))))
 
 ;;;###autoload
-(defun update-buffer-local-frame-params (&optional force-current-buffer)
-  (save-current-buffer
-    (unless force-current-buffer
-      (set-buffer (window-buffer (selected-window))))
+(defun update-buffer-local-frame-params (&optional buffer)
+  (with-current-buffer (if (buffer-live-p buffer)
+			   buffer
+			 (window-buffer (selected-window)))
     (update-buffer-local-cursor-color)
     (update-buffer-local-foreground-color)
     (update-buffer-local-background-color)))
@@ -275,8 +275,7 @@
 (add-hook 'minibuffer-setup-hook 'update-buffer-local-frame-params 'append)
 (add-hook 'minibuffer-exit-hook
 	  (lambda ()
-	    (with-current-buffer (nth 1 (buffer-list))
-	      (update-buffer-local-frame-params t)))
+	    (update-buffer-local-frame-params (nth 1 (buffer-list))))
 	  'append)
 
 (provide 'ccc)
