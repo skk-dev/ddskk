f19d75e6c6550ba15d6028b0063870ea9f0b8b1d
skk-cvs <skk-cvs>
2014-09-06 00:37:50 +0000

    skk.el (skk-save-jisyo): Add file-exists-p skk-jisyo for kill-emacs-hook.

--- skk.el
+++ skk.el
@@ -5,9 +5,9 @@
 
 ;; Author: Masahiko Sato <masahiko@kuis.kyoto-u.ac.jp>
 ;; Maintainer: SKK Development Team <skk@ring.gr.jp>
-;; Version: $Id: skk.el,v 1.633 2014/06/12 13:38:33 skk-cvs Exp $
+;; Version: $Id: skk.el,v 1.634 2014/09/06 00:37:50 skk-cvs Exp $
 ;; Keywords: japanese, mule, input method
-;; Last Modified: $Date: 2014/06/12 13:38:33 $
+;; Last Modified: $Date: 2014/09/06 00:37:50 $
 
 ;; This file is part of Daredevil SKK.
 
@@ -3506,7 +3506,13 @@ NOCLEAR が nil であれば送り仮名関連フラグを nil にセットする。
   ;; skk.el 以外で提供される辞書セーブ機能を利用できるように関数を funcall する
   ;; 形にしておく。
   (unless noninteractive
-    (funcall skk-save-jisyo-function quiet)))
+    ;; emacs の終了時に個人辞書を保存するため、skk.el (5190 行あたり) で
+    ;; kill-emacs-hook に #'skk-save-jisyo を add-hook している。
+    ;; skk-mode に入るたびに個人辞書の存在を確認している（なければ自動生成）が、
+    ;; 何らかの原因で個人辞書が存在しない状態で emacs を終了しようとすると
+    ;; skk-save-jisyo-original (skk-get-jisyo-buffer) 内のエラーで emacs を終了できない事態となる。
+    (when (file-exists-p skk-jisyo)
+      (funcall skk-save-jisyo-function quiet))))
 
 (defun skk-save-jisyo-original (&optional quiet)
   "SKK の辞書バッファをセーブする。
