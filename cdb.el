9e26201ae250a21ff1a296e3eca8894b605c88f8
skk-cvs <skk-cvs>
2014-12-01 07:54:45 +0000

    cdb.el: Fix file description.

--- cdb.el
+++ cdb.el
@@ -1,14 +1,51 @@
-;;  cdb.el - constant database (cdb) reader for Emacs Lisp.
+;;; cdb.el --- constant database (cdb) reader for Emacs Lisp
+
+;; Copyright (C) 2010 Yusuke Shinyama <yusuke at cs . nyu . edu>
+
+;; Author: Yusuke Shinyama <yusuke at cs . nyu . edu>
+;; Maintainer: SKK Development Team <skk@ring.gr.jp>
+;; Keywords: cdb
+;; Version: 20100719+
+;; Last Modified: $Date: 2014/12/01 07:54:45 $
+
+;; This file is not part of GNU Emacs.
+
+;; This is free and unencumbered software released into the public domain.
 ;;
-;;  Author: Yusuke Shinyama, 2010/07/19
-;;  * public domain *
+;; Anyone is free to copy, modify, publish, use, compile, sell, or
+;; distribute this software, either in source code form or as a compiled
+;; binary, for any purpose, commercial or non-commercial, and by any
+;; means.
+;;
+;; In jurisdictions that recognize copyright laws, the author or authors
+;; of this software dedicate any and all copyright interest in the
+;; software to the public domain. We make this dedication for the benefit
+;; of the public at large and to the detriment of our heirs and
+;; successors. We intend this dedication to be an overt act of
+;; relinquishment in perpetuity of all present and future rights to this
+;; software under copyright law.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+;; IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+;; OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;; OTHER DEALINGS IN THE SOFTWARE.
 ;; 
+;; For more information, please refer to <http://unlicense.org>
+
+;;; Commentary:
+
 ;;  Usage:
 ;;    (cdb-init "foo.cdb") -> nil
 ;;    (cdb-get "foo.cdb" "abc") -> "123"
 ;;    (cdb-keys "foo.cdb") -> ("abc" ...)
 ;;    (cdb-uninit "foo.cdb") -> nil
 ;;
+
+;;; Code:
+
 (eval-when-compile
   (require 'cl))
 
@@ -237,3 +274,5 @@
   (cdb-uninit path)
   )
 ;(cdb-test-simple "test.cdb")
+
+;;; cdb.el ends here
