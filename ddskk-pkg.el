7996a62eb5d01398d8f4ce4ec5a0329d38d5e3ba
Tsuyoshi Kitamoto <tsuyoshi.kitamoto@gmail.com>
2015-01-01 10:44:49 +0900

    Add comment

--- ddskk-pkg.el
+++ ddskk-pkg.el
@@ -1,3 +1,10 @@
+;;; ddskk-pkg.el --- define ddskk for package.el
+
+;;; GNU Emacs Lisp Reference Manual
+;;;   39 Preparing Lisp code for distribution
+;;;     39.3 Multi-file Packages
+;;;       One of the files in the content directory must be named name-pkg.el.
+
 (define-package "ddskk" "16.0.50"
   "Simple Kana to Kanji conversion program."
   '((cdb "20141201.754")))		; REQUIREMENTS
