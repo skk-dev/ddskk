3c98608af9a410666076fbe235ce93ed88f63e2c
Tsuyoshi Kitamoto <tsuyoshi.kitamoto@gmail.com>
2017-03-14 23:17:57 +0900

    modify
    * 速度優先のため上限 100 とする

--- experimental/skk-pre-henkan.el
+++ experimental/skk-pre-henkan.el
@@ -42,12 +42,13 @@
 (setq skk-dcomp-activate nil)
 (setq skk-completion-prog-list '((skk-pre-henkan)))
 
+;; internal variable.
 (defvar skk-pre-henkan-candidates nil)
 
 (defun skk-pre-henkan ()
-  "."
-  ;; skk-pre-henkan-candidates の先頭の car を返す。skk-pre-henkan-candidates は縮む。
-  ;; skk-comp-first が t なら、新たな skk-pre-henkan-candidates を作る。
+  "リスト `skk-completion-prog-list' の要素として使用."
+  ;; `skk-pre-henkan-candidates' の car を返す。`skk-pre-henkan-candidates' は縮む。
+  ;; `skk-comp-first' が t なら、新たな `skk-pre-henkan-candidates' を作る。
   (unless (string= skk-comp-key "")
     (when skk-comp-first
       (setq skk-pre-henkan-candidates (skk-pre-henkan-make-candidates)))
@@ -56,19 +57,22 @@
       (setq skk-pre-henkan-candidates (cdr skk-pre-henkan-candidates)))))
 
 (defun skk-pre-henkan-make-candidates ()
-  "Skk-comp-key をキー（先頭一致）として、候補のリストを返す."
+  "`skk-comp-key' をキー（先頭一致）として、候補のリストを返す."
   (let ((list-jisyo '(skk-jisyo skk-large-jisyo))
 	;; skk-comp-key は buffer-local なので with-current-buffer() 内では nil になる。
-	(key (car (split-string skk-comp-key "*" t)))
+	(key (format "^%s.* /" (car (split-string skk-comp-key "*" t))))
+	(i 0)
 	candidates)
     (dolist (jisyo list-jisyo)
       (with-current-buffer (skk-get-jisyo-buffer (symbol-value jisyo) 'nomsg)
 	(goto-char skk-okuri-nasi-min)
-	(while (re-search-forward (format "^%s.* /" key) nil t)
-	  (setq candidates (concat candidates
-				   (buffer-substring-no-properties (point)
-								   (progn (end-of-line)
-									  (point))))))))
+	  (while (and (re-search-forward key nil t)
+		      (< i 100))
+	    (setq candidates (concat candidates
+				     (buffer-substring-no-properties (point)
+								     (progn (end-of-line)
+									    (point)))))
+	    (setq i (1+ i)))))
     (when candidates
       (split-string candidates "/" t))))
 
